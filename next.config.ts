613c12fadfa86958420e65611ffc279989817b8a
NhatBangLe <bangle37568@gmail.com>
2025-07-29 16:17:25 +0700

    add patterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	images: {
-		remotePatterns: [new URL('https://bangnhatle.id.vn/api/v1/images/**')],
+		remotePatterns: [
+			new URL('https://bangnhatle.id.vn/api/v1/images/**'),
+			new URL('http://127.0.0.1:8000/api/v1/images/**'),
+		],
 	},
 };
 
