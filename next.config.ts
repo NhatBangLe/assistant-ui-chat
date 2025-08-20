c0654929c6902c02bde1e6be1393415f9318effd
NhatBangLe <bangle37568@gmail.com>
2025-08-20 20:41:08 +0700

    update configs

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [
-			new URL('https://bangnhatle.id.vn/threads/**'),
-			new URL('http://bangnhatle.id.vn/threads/**'),
-			new URL('https://agent.bangnhatle.id.vn/threads/**'),
 			new URL('https://127.0.0.1:8000/threads/**'),
 			new URL('http://127.0.0.1:8000/threads/**'),
 			new URL('https://127.0.0.1:8080/threads/**'),
